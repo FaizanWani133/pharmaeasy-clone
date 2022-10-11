3846150be7a3750d41de4d362cdd04c6708e4100
FAIZAN MUKHTAR <97751853+FaizanWani133@users.noreply.github.com>
2022-10-11 11:40:46 +0530

    banner

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,18 @@ import { Box } from '@chakra-ui/react'
 import React from 'react'
 import TabCarousal from '../Components/Home/TabCarousal'
 import SearchBar from '../Components/Home/SearchBar'
+import CategoryCraousal from '../Components/Home/CategoryCraousal'
+import Banner from '../Components/Home/Banner'
+
 
 function Home() {
   return (
     <Box bg="radial-gradient(150px 150px at 95% 0%, rgba(253, 186, 43, 0.3) 0%, rgba(253, 186, 43, 0) 100%), radial-gradient(150px 150px at 5% 0%, rgba(120, 213, 242, 0.3) 0%, rgba(253, 186, 43, 0) 100%)">
       <SearchBar/>
       <TabCarousal/>
+      <CategoryCraousal/>
+      <Banner/>
+      
     </Box>
       
     
