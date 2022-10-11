d7f0d74c6e505b9a7c55cefaae40855ce969ef8a
mdansari20101 <mdansari20101@gmail.com>
2022-10-11 17:30:28 +0530

    added slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Box, Container, Text } from '@chakra-ui/layout';
 import Footer from './Components/Footer/Footer';
-
+import Slider from './Components/Slider/Slider';
+import "./App.css";
 function App() {
   return (
     <Box width="100%" className="App">
       <Footer/>
+      <Slider/>
     </Box>
   );
 }
