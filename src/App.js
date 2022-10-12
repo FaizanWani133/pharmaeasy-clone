b26d41b7eee606dc0075170c7fec97ee115f5e5e
mdansari20101 <mdansari20101@gmail.com>
2022-10-12 17:36:17 +0530

    banner added

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ import AllRoutes from './Pages/AllRoutes';
 import Footer from './Components/Footer/Footer';
 import Slider from './Components/Slider/Slider';
 import "./App.css";
+import Banner from './Components/Footer/Banner';
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <AllRoutes/>
+      <Banner/>
       <Footer/>
+      
       {/* <Slider/> */}
     </div>
   );
