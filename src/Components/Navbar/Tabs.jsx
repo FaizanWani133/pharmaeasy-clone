c0564a983016a26edacdfbbc16daf2f5e47fb86d
FAIZAN MUKHTAR <97751853+FaizanWani133@users.noreply.github.com>
2022-10-13 00:04:41 +0530

    spelling changes

--- src/Components/Navbar/Tabs.jsx
+++ src/Components/Navbar/Tabs.jsx
@@ -1,4 +1,4 @@
-import { Box,  Flex, HStack, Text } from '@chakra-ui/react'
+import { Box, HStack, Text } from '@chakra-ui/react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {tabCards} from "../Home/TabCarousal"
