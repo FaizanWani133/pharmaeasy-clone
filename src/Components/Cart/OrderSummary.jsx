703312b7ded5f8746d389735f06934450158fc54
FAIZAN MUKHTAR <97751853+FaizanWani133@users.noreply.github.com>
2022-10-15 15:26:07 +0530

    abcdc

--- src/Components/Cart/OrderSummary.jsx
+++ src/Components/Cart/OrderSummary.jsx
@@ -1,16 +1,38 @@
-import { Box, Button, Divider, HStack, Text, VStack } from '@chakra-ui/react'
+import { Accordion,AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Button, Divider, Flex, HStack, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { TbDiscount2 } from 'react-icons/tb'
+
 
 function OrderSummary() {
   return (
-    <Box>
-        <VStack>
-            <Button width="100%">Apply Coupon</Button>
-            <Button>Proceed To Buy</Button>
+    <Box >
+        <VStack spacing={"20px"}>
+        <Accordion  allowMultiple width={"100%"} >
+  <AccordionItem borderRadius="5px" border={"1px solid teal"} padding="1px">
+    <h2>
+      <AccordionButton >
+        <Flex flex='1' textAlign='left' alignItems={"center"} color={"teal"} fontWeight="700">
+        <TbDiscount2/><Text ml={"10px"}>Apply Coupon</Text> 
+        </Flex>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+        asdasdadadadad
+      
+    </AccordionPanel>
+  </AccordionItem>
+
+  
+</Accordion>
+            <Button width={"100%"} colorScheme={"teal"}>Proceed To Buy</Button>
             <Divider/>
-            <Text> Order Summary</Text>
-            <HStack width={"100%"} justify={"space-between"}><Text>Cart Value</Text><Text>$ 500</Text></HStack>
-            <HStack width={"100%"} justify={"space-between"}><Text>Amount to be paid</Text><Text>$ 500</Text></HStack>
+            <VStack width={"100%"} >
+            <Text width={"100%"} textAlign={"left"} fontWeight="500"> Order Summary</Text>
+            <HStack width={"100%"} justify={"space-between"}><Text fontSize={"14px"}>Cart Value</Text><Text>$ 500</Text></HStack>
+            <HStack width={"100%"} justify={"space-between"}><Text fontSize={"14px"}>Amount to be paid</Text><Text>$ 500</Text></HStack>
+            </VStack>
+            
         </VStack>
     </Box>
   )
