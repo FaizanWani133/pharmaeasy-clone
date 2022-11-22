c796cd93c5413d1e896ff21b72a106ab6ca160ad
FAIZAN MUKHTAR <97751853+FaizanWani133@users.noreply.github.com>
2022-11-22 17:01:23 +0530

    backed clear

--- src/Redux/Cart/actionTypes.js
+++
@@ -1,8 +0,0 @@
-export const CLEAR_CART = 'CLEAR_CART'
-export const REMOVE = 'REMOVE'
-export const UPDATE = "UPDATE"
-export const GET_TOTAL = "GET_TOTAL"
-
-export const SET_CART = "SET_CART"
-export const ADD_ITEM = "ADD_ITEM"
-
