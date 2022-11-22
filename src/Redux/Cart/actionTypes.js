4ccf0976a247f8cfc3412d956feb8e9a91f2bb3e
FAIZAN MUKHTAR <97751853+FaizanWani133@users.noreply.github.com>
2022-11-22 17:04:00 +0530

    Revert "backed clear"

---
+++ src/Redux/Cart/actionTypes.js
@@ -0,0 +1,8 @@
+export const CLEAR_CART = 'CLEAR_CART'
+export const REMOVE = 'REMOVE'
+export const UPDATE = "UPDATE"
+export const GET_TOTAL = "GET_TOTAL"
+
+export const SET_CART = "SET_CART"
+export const ADD_ITEM = "ADD_ITEM"
+
